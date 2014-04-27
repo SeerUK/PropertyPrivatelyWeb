5a13233af6bcbd404aa5d1f99cb65a52b768a0e5
Elliot Wright <wright.elliot@gmail.com>
2014-04-27 22:37:34 +0100

    Added Guzzle.

--- src/src/PropertyPrivately/SecurityBundle/Controller/DefaultController.php
+++ src/src/PropertyPrivately/SecurityBundle/Controller/DefaultController.php
@@ -1,13 +1,25 @@
 <?php
 
+/**
+ * Property Privately Web
+ *
+ * (c) Elliot Wright, 2014 <wright.elliot@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace PropertyPrivately\SecurityBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+/**
+ * Authentication Controller
+ */
+class AuthenticationController extends Controller
 {
-    public function indexAction($name)
+    public function loginAction()
     {
-        return $this->render('PropertyPrivatelySecurityBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('PropertyPrivatelySecurityBundle:Default:index.html.twig');
     }
 }
